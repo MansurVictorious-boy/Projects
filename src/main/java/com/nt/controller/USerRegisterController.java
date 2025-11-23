e6b1684fb6efbfec378f93a761533851de68c341
MansurVictorious-boy <tinkutiti7@gmail.com>
2025-11-23 16:45:20 +0530

    adding Crossorigin on the top of usergister api

--- src/main/java/com/nt/controller/USerRegisterController.java
+++ src/main/java/com/nt/controller/USerRegisterController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,7 +35,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 
 @Tag(name = "UserRegisterController ",description = "UserRegister Regsiter and Login")
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class USerRegisterController {
